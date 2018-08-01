0d0bbb974e64fd0008865c1c88622a1f6a84c0cf
kei2100 <kei.arima.2100@gmail.com>
2018-08-01 23:48:15 +0900

    Add flags for the destination config

--- cli/root.go
+++ cli/root.go
@@ -12,5 +12,21 @@ var RootCmd = &cobra.Command{
 	Short: "fwxy is a simple HTTP forward proxy",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("test")
+		fmt.Println(rewritePaths)
+		fmt.Println(len(rewritePaths))
 	},
+}
+
+var (
+	username     string
+	password     string
+	rewritePaths []string
+)
+
+func init() {
+	flags := RootCmd.PersistentFlags()
+
+	flags.StringVarP(&username, "username", "u", "", "username for the basic authentication")
+	flags.StringVarP(&password, "password", "p", "", "password for the basic authentication")
+	flags.StringSliceVarP(&rewritePaths, "rewrite", "r", []string{}, "list for path rewrite (-r /old:/new -r /o:/n OR -r /old:/new,/o:/n)")
 }
